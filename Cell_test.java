a9c555bd1d0b51abe4c86438cba3d465d7016bf3
Kawamoto Naoyuki <n-kawamoto@cresco.co.jp>
2020-06-09 15:35:30 +0900

    a

--- Cell_test.java
+++ Cell_test.java
@@ -1,18 +1,29 @@
 package mini;
 
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
 import org.junit.Test;
+
+
 
 public class Cell_test {
 	@Test
 	public void test1_1() {
 		Cell cell1 = new Cell(1,1);
 		cell1.setMark(0);
-		System.out.println(cell1.getMark());
+		int actual = cell1.getMark();
+		assertThat(actual,is(0));
 	}
 	@Test
 	public void test1_2() {
 		Cell cell1 = new Cell(1,1);
-		System.out.println(cell1.getMark());
+		int actual = cell1.getMark();
+		assertThat(actual,is(2));
 	}
 
 }
+
+	
+
+
