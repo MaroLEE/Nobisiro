490f2bc6dc3d1dc129276d6c0b6129fe982663a3
MaroLEE <lee-maro@cresco.co.jp>
2020-06-13 18:49:14 +0900

    update

--- 第2反復_実装/Turn.java
+++ 第2反復_実装/Turn.java
@@ -1,5 +1,36 @@
 package mini;
 
 public class Turn {
+	private Player player;
+	private Cell cell;
+	private int mark;
+	
+	private int x;
+	private int y;
 
+	public Turn (Player nowPlayer) {
+		
+	}
+	
+	public void turnStart() {
+		cell.setCoordinates();
+	
+		Board.checkWin(){
+		System.out.println()
+		}
+		Board.chekcFull(){
+			
+		}
+	}
+	
+	public void showHistory() {
+		System.out.print(player.getPlayer() + ":(" + cell.getX() + "," + cell.getY() + ") ");
+		int mark = cell.getMark();
+		if(mark == 0) {
+			System.out.println("○");
+		}
+		else if(mark == 1) {
+			System.out.println("×");
+		}
+	}
 }
