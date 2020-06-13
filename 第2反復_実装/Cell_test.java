159ed20baca8c3668a8d2cbfe0c1b817a7785143
Abe Ruka <r-abe@cresco.co.jp>
2020-06-13 18:09:29 +0900

    第二反復ユニットテスト（cell）　

--- 第2反復_実装/Cell_test.java
+++ 第2反復_実装/Cell_test.java
@@ -1,18 +1,36 @@
 package mini;
 
-	import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.Test;
 
 	public class Cell_test {
 		@Test
 		public void test1_1() {
 			Cell cell1 = new Cell(1,1);
 			cell1.setMark(0);
-			System.out.println(cell1.getMark());
+			int actual = cell1.getMark();
+			assertThat(actual, is(0));
 		}
 		@Test
 		public void test1_2() {
 			Cell cell1 = new Cell(1,1);
-			System.out.println(cell1.getMark());
+			int actual = cell1.getMark();
+			assertThat(actual, is(2));
 		}
-
+		
+		@Test
+		public void test1_3() {
+			Cell cell1 = new Cell(1,2);
+			int actual = cell1.getX();
+			assertThat(actual, is(1));
+		}
+		
+		@Test
+		public void test1_4() {
+			Cell cell1 = new Cell(1,2);
+			int actual = cell1.getY();
+			assertThat(actual, is(2));
+		}
 	}
