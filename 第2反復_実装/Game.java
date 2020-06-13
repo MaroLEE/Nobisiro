48728b698f684f170e27524802e5b26daa795efd
Abe Ruka <r-abe@cresco.co.jp>
2020-06-13 17:17:17 +0900

    game

--- 第2反復_実装/Game.java
+++ 第2反復_実装/Game.java
@@ -2,43 +2,4 @@ package mini;
 
 public class Game {
 
-	private Player first;
-	private Player second;
-	private Player nowPlayer = second;
-	
-	public Game(){
-		this.first = new Player();
-		this.second = new Player();
-	}
-	
-	public void setPlayer(){
-		if(nowPlayer == first){//countを使ったループが必要かも、スイッチケース文、ifとか
-		this.nowPlayer = second;
-		}
-		this.nowPlayer = first;
-	}
-	
-	public void gameStart(){
-		for() {
-		Board board = new Board();
-		board.showBoard();//Boardクラスで盤の表示をする仕組みを作っておく
-		
-		setPlayer();
-		
-		Turn turn = new Turn(Player name);
-		turn.turnStart()；
-		}
-		
-		board.showBoard();
-		
-		for() {
-			return turn.showHistory();
-		}
-	}
-	
-	
-	
-	
-	
-	
 }
